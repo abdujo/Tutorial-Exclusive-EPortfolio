c5d5c49cca92d19b193b6f3f3db803c093a3d708
Kojo <kabdujo@gmail.com>
2025-06-13 12:22:25 -0400

    Myupdate

--- index.js
+++ index.js
@@ -16,22 +16,47 @@
 //         )
 // }
 
-
+// learn more about the event.preventDefault() method
+// what is const document querySelector
+// classList.add and remove
+// send form method
+// event.target
+// then fuction and catch function
+// the let function
+// the onclick function
 function contact(event) {
     event.preventDefault();
-    //     emailjs
-    //         .sendForm('service_wljzwur', 'template_amvqrzt', event.target,
-    //             'P2iXEfxavJX4fyh-L'
-    //         ).then(() => {
-    //             console.log('this worked!')
-    //         }
-
-    //         )
 
     const loading = document.querySelector('.modal__overlay--loading');
     const success = document.querySelector('.modal__overlay--success');
-    loading.classList += ' .modal__overlay--visible'
-    setTimeout(() => {
-        console.log('it worked!')
-    }, 1000);
+    loading.classList.add('modal__overlay--visible');
+
+
+    emailjs
+        .sendForm('service_wljzwur', 'template_amvqrzt', event.target,
+            'P2iXEfxavJX4fyh-L'
+        ).then(() => {
+            loading.classList.remove('modal__overlay--visible');
+            success.classList.add('modal__overlay--visible');
+        }
+
+        ).catch(() => {
+            loading.classList.remove('modal__overlay--visible');
+            alert('The email service is temporarily unavailable. Please contact me directly on my email.');
+        });
+
+
+
+}
+
+let isModalOpen = false;
+function toggleModal() {
+   if( isModalOpen) {
+        isModalOpen = false;
+       return document.body.classList.remove('modal--open');
+        
+    }
+isModalOpen = true;
+document.body.classList.add('modal--open');
+
 }
